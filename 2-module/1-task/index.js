1d02249aa6e35418b5bdb646f045497b8a5243b3
Vitalii Oberman <vitalii.oberman@pwc.ru>
2022-04-29 12:51:31 +0400

    module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,7 +2,7 @@ function sumSalary(salaries) {
   let sum = 0;
 
   for (let i in salaries) {
-    if (isFinite(salaries[i])) sum += salaries[i];
+    if (Number.isInteger(salaries[i])) sum += salaries[i];
   }
 
   return sum;
