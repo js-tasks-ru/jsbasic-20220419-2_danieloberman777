91e8251777d0b4873eca2af37dbe279cf08ba6d6
Vitalii Oberman <vitalii.oberman@pwc.ru>
2022-04-29 12:45:11 +0400

    task 3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for (let i in salaries) {
+    if (isFinite(salaries[i])) sum += salaries[i];
+  }
+
+  return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = String(a);
+    this.b = String(b);
+  },
+  sum() {
+    return Number(this.a) + Number(this.b);
+  },
+  mul() {
+    return Number(this.a) * Number(this.b);
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
